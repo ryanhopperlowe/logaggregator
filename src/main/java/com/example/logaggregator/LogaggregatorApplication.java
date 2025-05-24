3183df7b359a7adfacba09eca47312c247e01fe8
Ryan Hopper-Lowe <46546486+ryanhopperlowe@users.noreply.github.com>
2025-05-23 21:09:42 -0500

    initial commit

---
+++ src/main/java/com/example/logaggregator/LogaggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.logaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogaggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogaggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/logaggregator/LogaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
